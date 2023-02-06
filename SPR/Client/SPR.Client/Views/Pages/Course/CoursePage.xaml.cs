3924a2f509d08e1e15de8bff79d9f6e81aff18f6
Denis Kulakovskiy <70133789+denforplay@users.noreply.github.com>
2023-02-06 23:24:47 +0300

    Ui improvements, add resources

---
+++ SPR/Client/SPR.Client/Views/Pages/Course/CoursePage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SPR.Client.Views.Pages.Course;
+
+public partial class CoursePage : Page
+{
+    public CoursePage()
+    {
+        InitializeComponent();
+    }
+}
